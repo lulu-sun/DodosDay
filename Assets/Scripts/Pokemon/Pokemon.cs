49a7465371511921938667aca538f3a6409f3f76
lulu-sun <lulusun18@gmail.com>
2022-04-17 03:57:44 -0700

    pokemon battle scene setup

---
+++ Assets/Scripts/Pokemon/Pokemon.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pokemon
+{
+    PokemonBase _base;
+    int level;
+
+    public int HP {
+        get;
+        set;
+
+    }
+
+    public List<Move> Moves {
+        get;
+        set;
+
+    }
+
+    public Pokemon(PokemonBase pBase, int pLevel)
+    {
+        _base = pBase;
+        level = pLevel;
+        HP = _base.MaxHp;
+
+        Moves = new List<Move>();
+        foreach (var move in _base.LearnableMoves)
+        {
+            if (move.Level <= level)
+            {
+                Moves.Add(new Move(move.Base));
+            }
+
+            if (Moves.Count >= 4)
+            {
+                break;
+            }
+        }
+    }
+
+    public int Attack {
+        get { 
+            return Mathf.FloorToInt((_base.Attack * level) / 100f) + 5; }
+    }
+
+    public int MaxHp {
+        get {
+            return Mathf.FloorToInt((_base.MaxHp * level) / 100f) + 10;}
+    }
+
+    public int Defense {
+        get {
+            return Mathf.FloorToInt((_base.Defense * level) / 100f) + 5;}
+    }
+    public int SpAttack {
+        get {
+            return Mathf.FloorToInt((_base.SpAttack * level) / 100f) + 5;}
+    }
+    public int SpDefense {
+        get {
+            return Mathf.FloorToInt((_base.SpDefense * level) / 100f) + 5;}
+    }
+    public int Speed {
+        get {
+            return Mathf.FloorToInt((_base.Speed * level) / 100f) + 5;}
+    }
+
+}
--- Assets/Scripts/Pokemon/PokemonBase.cs
+++ Assets/Scripts/Pokemon/PokemonBase.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [CreateAssetMenu(fileName = "Pokemon", menuName = "Pokemon/Create new pokemon")]
 
-public class NewBehaviourScript : ScriptableObject
+public class PokemonBase : ScriptableObject
 {
 
     [SerializeField] string name;
@@ -25,6 +26,80 @@ public class NewBehaviourScript : ScriptableObject
     [SerializeField] int spAttack;
     [SerializeField] int spDefense;
     [SerializeField] int speed;
+    [SerializeField] List<LearnableMove> learnableMoves;
+
+
+    public string Name {
+        // this is a property
+        get { return name; }
+    }
+
+    public string Description {
+        get { return description; }
+    }
+
+    public Sprite FrontSprite {
+        get {return frontSprite; }
+    }
+    public Sprite BackSprite {
+        get {return backSprite; }
+    }
+
+    public PokemonType Type1 {
+        get {return type1; }
+    }
+    public PokemonType Type2 {
+        get {return type2; }
+    }
+
+    // Base Stats
+    public int MaxHp {
+        get {return maxHp; }
+    }
+    public int Attack {
+        get {return attack; }
+    }
+    public int Defense {
+        get {return defense; }
+    }
+    public int SpAttack {
+        get {return spAttack; }
+    }
+    public int SpDefense {
+        get {return spDefense; }
+    }
+    public int Speed {
+        get {return speed; }
+    }
+
+    public List<LearnableMove> LearnableMoves {
+        get {
+            return learnableMoves;
+        }
+    }
+
+
+}
+
+[System.Serializable]
+
+public class LearnableMove
+{
+    [SerializeField] MoveBase moveBase;
+    [SerializeField] int level;
+
+    public MoveBase Base {
+        get {
+            return moveBase;
+        }
+    }
+
+    public int Level {
+        get {
+            return level;
+        }
+    }
+
 }
 
 public enum PokemonType
